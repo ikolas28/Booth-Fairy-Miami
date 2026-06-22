e74e369df0ac5582526905784634dc8dd74ef757
Andy Gutierrez <andygutierrez68@gmail.com>
2026-06-21 23:03:10 -0400

    Add GA4 lead tracking

--- website/tracking.js
+++ website/tracking.js
@@ -1,4 +1,26 @@
 (function () {
+  const googleMeasurementId = "G-8D92QEYZ5H";
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
+
+  const googleScript = document.createElement("script");
+  googleScript.async = true;
+  googleScript.src = `https://www.googletagmanager.com/gtag/js?id=${googleMeasurementId}`;
+  document.head.appendChild(googleScript);
+
+  window.gtag("js", new Date());
+  window.gtag("config", googleMeasurementId);
+
+  if (window.location.pathname.endsWith("/thank-you.html")) {
+    window.gtag("event", "generate_lead", {
+      event_category: "engagement",
+      event_label: "website_inquiry",
+    });
+  }
+
   if (window.fbq) return;
 
   const fbq = (window.fbq = function () {
